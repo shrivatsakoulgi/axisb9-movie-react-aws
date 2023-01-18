28edf382bd06b8ffcd150c9dee6baab496a2db0b
shrivatsakoulgi <89825785+shrivatsakoulgi@users.noreply.github.com>
2023-01-18 17:36:06 +0530

    Update AllMovies.js

--- src/pages/AllMovies.js
+++ src/pages/AllMovies.js
@@ -6,7 +6,7 @@ class AllMovies extends Component {
     };
 
     //baseURL="http://localhost:8300/movies"
-    baseURL="http://ec2-54-157-169-238.compute-1.amazonaws.com:8300/movies"
+    baseURL="https://ec2-54-157-169-238.compute-1.amazonaws.com:8300/movies"
     async componentDidMount(){
         const response = await fetch(this.baseURL);
         const body=await response.json();
